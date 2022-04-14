f8f245d9baff19f64ce0691563c75970f552fe07
Pranav Soni <pranavsoni1101@gmail.com>
2022-04-14 10:17:47 +0530

    Added filecheck script

---
+++ fileCheck.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+f=$1
+
+if [ -d $f  ]
+then    echo "directory"
+
+elif [ -f $f  ]
+then    
+    echo "file"
+
+else
+    echo "invalid"
+    exit 0
+
+fi
+
+if [ -x $f ]
+then    
+    echo "ecevutable"
+fi
