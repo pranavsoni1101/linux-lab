953a9d7ae927345f5c49cf8e2180498bf161cf9b
Pranav Soni <pranavsoni1101@gmail.com>
2022-04-21 09:47:49 +0530

    Added logic to functions file

--- functions.sh
+++ functions.sh
@@ -1,3 +1,34 @@
+#!/bin/bash
+
 # Syntax of function:
 # function_name () { }
 # to invoke the fn just type in the fn name
+# to return a value from a fn, it is stored in a special variable '$?'
+a=$1
+b=$2 
+
+add () {
+    echo "addition $(( $a + $b))"
+}
+
+subtraction () {
+    echo "subtraction $(( $a - $b))"
+}
+
+multiplication () {
+    echo "multiplication $(( $a * $b))"
+}
+
+division () {
+    echo "division $(( $a / $b))"
+}
+
+modulous () {
+    echo "modulous $(( $a % $b))"
+}
+
+add
+subtraction
+multiplication
+division
+modulous
