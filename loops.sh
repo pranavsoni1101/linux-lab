a3912df740e4876720abc6906ec16909d91b9d48
Pranav Soni <pranavsoni1101@gmail.com>
2022-04-21 10:01:47 +0530

    Added logic for loop file

--- loops.sh
+++ loops.sh
@@ -1,3 +1,4 @@
+#!/bin/bash/
 # the 3 types of loops are for, while, until
 
 # Syntax for loop: 
@@ -7,7 +8,7 @@
 # done
 
 # Syntax while loop:
-# wgule [ condition ]
+# while [ condition ]
 # do
 # ........
 # done
@@ -17,3 +18,40 @@
 # do
 # ......
 # done
+
+# Using for loop
+echo "using a for loop"
+for a in {1..20} 
+do
+    b=$(($a % 2))
+    if [[ $b -eq 1 ]]
+    then
+        echo "$a"
+    fi
+done
+
+# Using while loop
+echo "using a while loop"
+a=1
+while [[ $a -lt 20 ]]
+do  
+    b=$(( $a % 2 ))
+    if [[ $b -eq 1 ]]
+    then
+        echo "$a"
+    fi
+    a=$(( $a + 1 ))
+done
+
+# Using until loop
+echo "using a until loop"
+a=1
+until [[ $a -gt 20 ]]
+do  
+    b=$(( $a % 2 ))
+    if [[ $b -eq 1 ]]
+    then
+        echo "$a"
+    fi
+    a=$(( $a + 1 ))
+done
